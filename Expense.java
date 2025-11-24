aa7913cbb2943bf718c43daff09924eb608b578a
Surya07-07 <nagula.24mim10086@vitbhopal.ac.in>
2025-11-24 10:57:20 +0530

    Add files via upload

---
+++ Expense.java
@@ -0,0 +1,27 @@
+public class Expense {
+    private String description;
+    private String category;
+    private double amount;
+
+    public Expense(String description, String category, double amount) {
+        this.description = description;
+        this.category = category;
+        this.amount = amount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public String toString() {
+        return description + " | " + category + " | ₹" + amount;
+    }
+}
